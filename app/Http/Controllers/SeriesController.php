4a317cd9efe76c4b816ffdc52e5c32f1ef7dc992
Matheus Bueno <bueno_matheus@outlook.com>
2022-01-10 09:05:46 -0300

    Laravel primeiras alterações

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller{
+    public function index(Request $request) {
+         $series = [
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+    /*Retornar a view e compact para passar uma variavel do controller para a view no caso o array series */
+        return view('series.index', compact('series'));
+    }
+}
