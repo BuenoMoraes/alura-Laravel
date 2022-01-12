336a36dedeed1eca3640f698de97b3dd68b0b1f6
Matheus Bueno <bueno_matheus@outlook.com>
2022-01-12 10:17:29 -0300

    Alterações aula 5 e 6

--- database/migrations/2022_01_11_164011_adiciona_campo_assistido.php
+++ database/migrations/2022_01_11_164011_adiciona_campo_assistido.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AdicionaColunaAssistido extends Migration
+class AdicionaCampoAssistido extends Migration
 {
     /**
      * Run the migrations.
